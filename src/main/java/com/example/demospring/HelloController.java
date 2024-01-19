c7cb72500778bc0eda000bd6ece02334c1153efb
heiler <threepebbles@naver.com>
2024-01-19 20:38:30 +0900

    Feat: 테스트 Controller 추가

---
+++ src/main/java/com/example/demospring/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demospring;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public ResponseEntity hello() {
+        return ResponseEntity.ok("eroom 배포 자동화 테스트");
+    }
+}
