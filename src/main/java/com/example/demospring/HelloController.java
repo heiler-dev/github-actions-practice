0b1e5b028e3266d313d905a554fa5338589baa55
threepebbles <threepebbles@naver.com>
2024-03-10 15:08:46 +0900

    refactor: hello API uri 변경

--- src/main/java/com/example/demospring/HelloController.java
+++ src/main/java/com/example/demospring/HelloController.java
@@ -2,12 +2,14 @@ package com.example.demospring;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/hello")
 public class HelloController {
     @GetMapping("/")
-    public ResponseEntity hello() {
+    public ResponseEntity<String> hello() {
         return ResponseEntity.ok("배포 자동화 테스트");
     }
 }
